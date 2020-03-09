0caf4cae45fab9d89571a7b64b57a12b8583cee5
Danny <danny.connolly@my.jcu.edu.au>
2020-03-09 21:01:54 +1100

    rectified scripting issue

--- app/src/main/java/au/edu/jcu/sp3406/quicksum/MainActivity.java
+++ app/src/main/java/au/edu/jcu/sp3406/quicksum/MainActivity.java
@@ -13,22 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView textView = findViewById(R.id.sum);
     }
     public void buttonClicked(View view){
         Button button = (Button) view;
         TextView textView = findViewById(R.id.sum);
-        if(button.getText().toString().equals("clear")){
-            textView.setText("");
-            sum = 0;
-        }
-        else {
             int number = Integer.parseInt(button.getText().toString());
             sum += number;
             String result = "" + sum;
             textView.setText(result);
         }
-    }
     public void clearButton(View view){
         TextView textView = findViewById(R.id.sum);
         textView.setText("");
